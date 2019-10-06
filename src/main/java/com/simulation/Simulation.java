0f26c5f06e0f95e08cfcf95d789fa1f47662b04b
sunilsyenare <syenare97@gmail.com>
2019-10-06 15:16:46 +0530

    Add. winning probability

--- src/main/java/com/simulation/Simulation.java
+++ src/main/java/com/simulation/Simulation.java
@@ -7,25 +7,12 @@ public class Simulation {
         this.numberOfTrial = numberOfTrial;
     }
 
-    public double calculateWinnningProbability(Trial trial) {
+    public double calculateWinningProbability() {
         int count=0;
         for(int i=0;i<numberOfTrial;i++){
-            if(trial.status())
-            {
-                count++;
-            }
+
         }
         return count/numberOfTrial;
     }
 
-    public double calculateLosProbability(Trial trial) {
-        int count=0;
-        for(int i=0;i<numberOfTrial;i++){
-            if(trial.status())
-            {
-                count++;
-            }
-        }
-        return count/numberOfTrial;
-    }
 }
