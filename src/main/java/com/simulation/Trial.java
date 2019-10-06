a464a8aa134fe2b461a0f6daf58bf613ed7f81bb
sunilsyenare <syenare97@gmail.com>
2019-10-06 14:38:24 +0530

    Add. trial test

---
+++ src/main/java/com/simulation/Trial.java
@@ -0,0 +1,33 @@
+package com.simulation;
+
+public class Trial {
+
+    // TODO - maybe primitive obsession
+    private final boolean doorA;
+    private final boolean doorB;
+    private final boolean doorC;
+    private int choice;
+
+    // TODO - too many parameters
+    public Trial(boolean doorA, boolean doorB, boolean doorC,int choice) {
+        this.doorA = doorA;
+        this.doorB = doorB;
+        this.doorC = doorC;
+        this.choice = choice;
+    }
+
+    boolean status() {
+        switch (choice) {
+            case 1:
+                return doorA;
+            case 2:
+                return doorB;
+            case 3:
+                return doorC;
+            default:
+                return false;
+        }
+    }
+
+
+}
